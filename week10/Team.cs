29c6610714ae27d5db95d6b299065082a0de19be
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2021-04-27 13:31:06 +0100

    Add project files.

---
+++ week10/Team.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week10
+{
+    public class Team
+    {
+
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public string Location { get; set; }
+
+        public virtual List<Player> Players { get; set; }
+
+    }
+
+    public class Player
+    {
+
+        public int PlayerID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public int TeamID { get; set; }
+        public virtual Team Team {get;set;}
+
+    }
+
+}
